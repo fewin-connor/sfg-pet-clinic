6bfa6aa5451b3a8f400fe58ec44abdc0b4464f05
fewin-connor <624883@bah.com>
2022-07-14 09:48:13 -0400

    Added Vet Specialty Entity. Closes #23.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
-import scala.collection.mutable.Set;
+import java.util.Set;
 
 public class Owner extends Person {
 
     private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
     
 
 }
