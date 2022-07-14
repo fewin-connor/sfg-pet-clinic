13b17731d38032a151b0eaf16f9fa0bd9de88398
fewin-connor <624883@bah.com>
2022-07-14 09:43:36 -0400

    Resolved some issues. Closes #22.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,34 +1,30 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDate;
 
-/**
- * Created by jt on 7/29/18.
- */
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-@Table(name = "visits")
 public class Visit extends BaseEntity {
 
-    @Column(name = "date")
-    private LocalDate date;
-
-    @Column(name = "description")
+    private LocalDate data;
     private String description;
-
-    @ManyToOne
-    @JoinColumn(name = "pet_id")
     private Pet pet;
-
-    public LocalDate getDate() {
-        return date;
+    
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Pet getPet() {
+        return pet;
+    }
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 
 }
